7efda3d495a8f457d130c766bdb5c67a7817c6ce
ParkashSuther <41523939+ParkashSuther@users.noreply.github.com>
2020-06-07 14:36:57 +0500

    Add files via upload

---
+++ chapter2/app.js
@@ -0,0 +1,29 @@
+// 1.
+var userName ;
+
+// 2. 
+var myName ="Parkash Suthar";
+
+// 3.
+var Message;
+Message="hello world"
+alert(document.title=Message);
+
+// 4.
+var sName="parkash";
+var age="22";
+var course="Certified Mobile Application Development";
+alert(sName);
+alert(age);
+alert(course);
+
+// 5.
+var pizza="pizza";
+var text;
+for (var i =pizza.length(); i<1;i--){
+    for(var j=0;j<i;j++){
+    text+=pizza.indexOf(i);
+    }
+    "<br>";
+}
+alert(text);
