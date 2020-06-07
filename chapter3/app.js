1d5175462c4da1ec8b290b1c67d313f8aa3d18d6
ParkashSuther <41523939+ParkashSuther@users.noreply.github.com>
2020-06-07 16:35:14 +0500

    Add files via upload

---
+++ chapter3/app.js
@@ -0,0 +1,17 @@
+// 1.
+var age=22;
+alert("i am "+age+" year old");
+
+// 2.
+
+// 3.
+var birthdate=2000;
+document.write("my birth year is "+birthdate+"<br>Data type of my ddeclared is number");
+
+
+// 4.
+var vName="parkash";
+var productTitle="book";
+var quantity=999;
+
+document.write("<h3>User Name:</h3> "+vName+"<h3>Product: </h3>"+productTitle+"<h3>Quantity: </h3>"+quantity+"")
