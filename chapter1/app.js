5291118e5f1e2c8936e443ed8accfc79fd1f8402
ParkashSuther <41523939+ParkashSuther@users.noreply.github.com>
2020-06-07 14:35:23 +0500

    Add files via upload

---
+++ chapter1/app.js
@@ -0,0 +1,16 @@
+
+// 1.
+// alert("welcome to our Website");
+
+// 2.
+// window.alert("error! please enter a valid password ");
+
+// 3.
+// alert("welcome to JS land... \nHappy coding!");
+
+// 4.
+// alert("welcome to JS land");
+
+
+//  7.
+// --> B
